284f2090b3950ced46756f0efc77d5e256ae010b
Puja3010 <puja.rout30@gmail.com>
2026-01-19 16:47:07 +0530

    project structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Puja3010",
+    author_email="puja.rout30@gmail.com",
+    packages=find_packages()
+)
